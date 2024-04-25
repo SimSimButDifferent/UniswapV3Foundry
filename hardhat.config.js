6610d0e91fe45d7c5a9a74fb403d850d055ea0b3
simsim <simeon.campbell@live.com>
2024-04-25 14:34:14 +0700

    config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox")
 require("@nomicfoundation/hardhat-foundry")
+require("dotenv").config()
+
+const ALCHEMY_MAINNET_API = process.env.ALCHEMY_MAINNET_API
 
 module.exports = {
     networks: {
         hardhat: {
             forking: {
-                url: "https://eth-mainnet.g.alchemy.com/v2/VuIQG_PWKLrGJ3At5UsTBFVSaq97E41r",
+                url: ALCHEMY_MAINNET_API,
                 blockNumber: 19709300,
                 gasLimit: 12000000,
             },
